1fac774198439342b2bb4f2ca8e04f34cb0f4691
Rhuan Marcos <rhuanoliveira2004@gmail.com>
2025-08-10 22:06:51 -0300

    primeiro commit

---
+++ main.js
@@ -0,0 +1,21 @@
+function game(params) {
+    
+    var jogador;
+    var bot;
+    var par;
+    var impar;
+    var mdireita;
+    var mesquerda;
+    var soma;
+    var r;
+
+    jogador = document.getElementById("jogador").value;
+
+    soma = mdireita + mesquerda
+
+    bot = Math.ceil(Math.random() * 10);
+
+    if (jogador == soma) {
+     
+    }
+}
